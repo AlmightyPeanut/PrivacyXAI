6ad364b33a0561112fc7edc16391fa39c37b756f
AlmightyPeanut <benedikthornig@outlook.com>
2024-03-20 12:49:09 +0100

    creating missing icd code embeddings by aggregating their group

--- experiments/dataset/data_reshaping/reshape_data.py
+++ experiments/dataset/data_reshaping/reshape_data.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import polars as pl
 import seaborn as sns
+from sklearn.metrics.pairwise import rbf_kernel
 
 DATA_DIR_ROOT = Path(__file__).parent.parent.parent.parent / 'data' / 'MIMIC'
 DATA_DIR_ORIGINAL = DATA_DIR_ROOT / 'original'
@@ -68,18 +69,55 @@ def reshape_omr_data() -> pl.DataFrame:
 
 def reshape_diagnoses_data() -> pl.DataFrame:
     diagnoses_data = pl.read_csv(DATA_DIR_ORIGINAL.joinpath('diagnoses_icd.csv'))
-    icd_code_embedings = pl.read_csv(DATA_DIR_ROOT / 'icd-10-cm-2022-0010.csv')
+    icd_code_embedings = pl.read_csv(DATA_DIR_ROOT / 'icd-10-cm-2022-1000.csv').select(pl.all().exclude('desc'))
 
+    # convert icd-9 to icd-10
     icd_code_mapper = Mapper()
     ICD_9_TO_10_MAP = partial(icd_code_mapper.map, source='icd9', target='icd10')
 
     diagnoses_data = diagnoses_data.with_columns(
         pl.when(pl.col('icd_version') == 9).then(pl.col('icd_code').map_elements(ICD_9_TO_10_MAP))
         .otherwise(pl.col('icd_code')).alias('icd_code'),
+        # pl.col('icd_code').alias('original_code')
     ).drop('icd_version')
 
+    # missing_codes = diagnoses_data.select(pl.col('original_code').filter(pl.col('icd_code').is_null()).unique().sort())
+    missing_code_count = diagnoses_data.select(
+        pl.col('icd_code').is_null().sum()
+    )['icd_code'][0]
+    print(f"Unable to convert {missing_code_count} codes to ICD-10. Dropping these samples.")
+    diagnoses_data = diagnoses_data.filter(pl.col('icd_code').is_not_null())
+
+    # For missing icd codes in the embedding aggregate all vectors of the next fitting icd code group
+    missing_vector_icd_codes = (
+        diagnoses_data
+        .select(pl.col('icd_code')
+                .filter(pl.col('icd_code')
+                        .is_in(icd_code_embedings.select(pl.col('code').unique())
+                               ).not_()).unique())
+    )
+
+    aggregated_missing_vector_icd_codes = []
+    for missing_vector_icd_code in missing_vector_icd_codes['icd_code']:
+        missing_embeddings = pl.DataFrame()
+        chars_to_remove = 0
+        while missing_embeddings.is_empty():
+            chars_to_remove += 1
+            missing_embeddings = icd_code_embedings.filter(
+                pl.col('code').str.starts_with(missing_vector_icd_code[:-chars_to_remove])
+            )
+
+        aggregated_missing_vector_icd_codes.append(missing_embeddings.select(
+            pl.lit(missing_vector_icd_code).alias('code'),
+            pl.all().exclude('code').max(),
+        ))
+
+    aggregated_missing_vector_icd_codes = pl.concat(aggregated_missing_vector_icd_codes)
+    icd_code_embedings = pl.concat([icd_code_embedings, aggregated_missing_vector_icd_codes])
+
+    # Add embeddings to the icd codes
     diagnoses_data = diagnoses_data.join(
-        icd_code_embedings.select(pl.all().exclude('desc')),
+        icd_code_embedings,
         left_on='icd_code', right_on='code', how='left')
 
     diagnoses_data = diagnoses_data.group_by(['subject_id', 'hadm_id']).agg([
@@ -106,12 +144,12 @@ def plot_admission_data(admission_data: pl.DataFrame) -> None:
 
     admission_plot_data = pl.concat([admission_plot_data_lower, admission_plot_data_upper], how='horizontal')
 
-    # sns.barplot(plot_data, color='#5975a4')
-    # plt.xticks(np.arange(0, 12, 1), plot_data.columns)
-    # plt.xlabel('Nights of Stays')
-    # plt.ylabel('Number of entries')
-    # plt.tight_layout()
-    # plt.show()
+    sns.barplot(admission_data, color='#5975a4')
+    plt.xticks(np.arange(0, 12, 1), admission_plot_data.columns)
+    plt.xlabel('Nights of Stays')
+    plt.ylabel('Number of entries')
+    plt.tight_layout()
+    plt.show()
 
 
 def get_length_of_stay(subject_and_hadm_ids: pl.DataFrame) -> pl.DataFrame:
@@ -126,9 +164,10 @@ def get_length_of_stay(subject_and_hadm_ids: pl.DataFrame) -> pl.DataFrame:
     # plot_admission_data(admission_data)
 
     admission_data = admission_data.with_columns(
-        nights_of_stay_group=pl.when(pl.col('nights_of_stay') < 10).then(pl.col('nights_of_stay'))
-        .when(pl.col('nights_of_stay') < 100).then(11)
-        .otherwise(12)
+        nights_of_stay_group=pl
+        .when(pl.col('nights_of_stay') < 3).then(0)
+        .when(pl.col('nights_of_stay') < 7).then(1)
+        .otherwise(2)
     )
 
     return admission_data
@@ -141,9 +180,10 @@ if __name__ == '__main__':
 
     all_data = diagnoses_data_reshaped.join(omr_data_reshaped, on='subject_id', how='left')
     all_data = all_data.join(length_of_stay, on=['subject_id', 'hadm_id'], how='inner')
+    # TODO: check for all null rows icd_code + vitals
 
     with pl.Config(tbl_cols=-1, tbl_width_chars=1000):
         print(all_data.describe())
         print(all_data.head(5))
 
-    all_data.write_csv(DATA_DIR_ROOT / 'mimic_data_processed.csv')
+    all_data.fill_null(-1).write_csv(DATA_DIR_ROOT / 'mimic_data_processed.csv')
