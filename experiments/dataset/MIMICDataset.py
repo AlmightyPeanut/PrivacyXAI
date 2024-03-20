0a3aac969d713390019be0356125afb6529295ea
AlmightyPeanut <benedikthornig@outlook.com>
2024-03-20 15:48:10 +0100

    batch get_item function for better performance

--- experiments/dataset/MIMICDataset.py
+++ experiments/dataset/MIMICDataset.py
@@ -1,6 +1,5 @@
-import os.path
+import numpy as np
 import polars as pl
-import torch
 
 from pathlib import Path
 from torch.utils.data import Dataset
@@ -14,35 +13,27 @@ class MIMICDataset(Dataset):
             ['icd_codes', 'subject_id', 'hadm_id', 'nights_of_stay'])).cast(pl.Float64)
 
         self.data_size = len(data)
-        self.features = data.select(pl.all().exclude('nights_of_stay_group')).transpose()
-        self.classes = data.select(pl.col('nights_of_stay_group')).transpose()
+
+        new_column_names = list(map(str, range(data.shape[0])))
+        self.features = data.select(pl.all().exclude('nights_of_stay_group')).transpose(column_names=new_column_names)
+        self.classes = data.select(pl.col('nights_of_stay_group')).transpose(column_names=new_column_names)
 
         self.transform = transform
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.data_size
 
-    def __getitem__(self, index: int) -> torch.Tensor:
-        features = self.features.select(pl.col(self.features.columns[index])).transpose().to_numpy()
-        classes = self.classes.select(pl.col(self.classes.columns[index])).transpose().to_numpy()
+    def __getitem__(self, index: int) -> np.array:
+        return self.__getitems__([index])
 
-        sample = {
-            'features': torch.tensor(features),
-            'classes': torch.tensor(classes)
-        }
-
-        if self.transform:
-            sample = self.transform(sample)
-
-        return sample
-
-    def __getitems__(self, index: list[int]) -> torch.Tensor:
-        features = self.features.select(pl.col([self.features.columns[i] for i in index])).transpose().to_numpy()
-        classes = self.classes.select(pl.col([self.features.columns[i] for i in index])).transpose().to_numpy()
+    def __getitems__(self, index: list[int]) -> np.array:
+        sample_selection = pl.col(list(map(str, index)))
+        features = self.features.select(sample_selection).transpose().to_numpy()
+        classes = self.classes.select(sample_selection).transpose().to_numpy().squeeze(1)
 
         samples = {
-            'features': torch.tensor(features),
-            'classes': torch.tensor(classes)
+            'features': features,
+            'classes': classes
         }
 
         if self.transform:
