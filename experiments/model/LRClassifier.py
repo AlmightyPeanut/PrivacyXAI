ed07b93723030d79cdc19161d3578211fec72e6b
AlmightyPeanut <benedikthornig@outlook.com>
2024-05-13 16:03:23 +0200

    updated init of LR, removed normalisation

--- experiments/model/LRClassifier.py
+++ experiments/model/LRClassifier.py
@@ -2,17 +2,23 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
+torch.manual_seed(42)
+
 
 class LRClassifier(nn.Module):
     def __init__(self, number_of_features: int, number_of_classes: int):
         super(LRClassifier, self).__init__()
+        self.number_of_classes = number_of_classes
 
         self.output_layer = nn.Linear(number_of_features, number_of_classes)
+        nn.init.uniform_(self.output_layer.weight, -0.1, 0.1)
+        nn.init.zeros_(self.output_layer.bias)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = F.normalize(x, dim=1)
-
         x = self.output_layer(x)
-        x = F.softmax(x, dim=-1)
+        if self.number_of_classes > 1:
+            x = F.softmax(x, dim=-1)
+        else:
+            x = F.sigmoid(x)
 
         return x
