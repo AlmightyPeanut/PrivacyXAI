60fcc0857853f2f33ef7c352d721d98eec2b1bd7
AlmightyPeanut <benedikthornig@outlook.com>
2024-06-11 15:50:00 +0200

    increase training epochs to 100

--- experiments/federated_learning/FederatedLearningConfig.py
+++ experiments/federated_learning/FederatedLearningConfig.py
@@ -9,7 +9,7 @@ class FederatedLearningConfig:
     # TODO: improve namings
     fraction_fit: float = 1.0
     fraction_evaluate: float = .0
-    num_rounds: int = 1
+    num_rounds: int = 100
 
     client_training_epochs: int = 1
     client_resources: dict = field(default_factory=lambda: {
--- experiments/model/ModelConfig.py
+++ experiments/model/ModelConfig.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass, field
 @dataclass
 class ModelConfig:
     target_models: set = field(default_factory=lambda: {'LR', 'NN'})
-    number_of_epochs: int = 20
+    number_of_epochs: int = 100
 
     dp_target_delta: float = 0.01
     dp_max_grad_norm: float = 1.0
