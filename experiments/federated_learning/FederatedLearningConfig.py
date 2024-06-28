3f2d0c0b1dd5bf7bd9bc0eca0df9654f4e529544
AlmightyPeanut <benedikthornig@outlook.com>
2024-06-28 21:05:03 +0200

    change FL parameters

--- experiments/federated_learning/FederatedLearningConfig.py
+++ experiments/federated_learning/FederatedLearningConfig.py
@@ -10,8 +10,9 @@ class FederatedLearningConfig:
     fraction_fit: float = 1.0
     fraction_evaluate: float = .0
     num_rounds: int = 100
+    num_rounds: int = 20
 
-    client_training_epochs: int = 1
+    client_training_epochs: int = 5
     client_resources: dict = field(default_factory=lambda: {
         "num_cpus": 20,
         "num_gpus": 1,
