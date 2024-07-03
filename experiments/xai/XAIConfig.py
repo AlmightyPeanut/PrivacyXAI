30027a86378a3b71f898e6db15f03a7e938dc7a5
AlmightyPeanut <benedikthornig@outlook.com>
2024-07-03 09:31:47 +0200

    configure xai metrics add multiprocessing to xai eval switch to quantus.evaluate instead of doing it myself

--- experiments/xai/XAIConfig.py
+++ experiments/xai/XAIConfig.py
@@ -1,6 +1,14 @@
-from dataclasses import dataclass, field
+import multiprocessing
+import torch
 
+from dataclasses import dataclass, field
 from captum._utils.models import SkLearnLasso, SkLearnLinearRegression
+from captum.attr import (
+    Lime,
+    IntegratedGradients,
+    FeaturePermutation,
+)
+
 from quantus import (
     FaithfulnessEstimate,
     Monotonicity,
@@ -21,44 +29,89 @@ from quantus import (
 
 from experiments.xai.CustomSensitivityN import CustomSensitivityN
 
+if torch.cuda.is_available():
+    TORCH_DEVICE = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    TORCH_DEVICE = torch.device("mps")
+else:
+    TORCH_DEVICE = torch.device("cpu")
+
 
 @dataclass
 class XAIConfig:
+    processes: int = multiprocessing.cpu_count() // 2
+    number_of_xai_test_samples: int = 5
     methods: dict = field(default_factory=lambda: {
-        # 'Lime+Lasso': ("Lime", {
-        #     "interpretable_model": SkLearnLasso(alpha=.0)
-        # }),
-        # 'Lime+LR': ("Lime", {
-        #     "interpretable_model": SkLearnLinearRegression()
-        # }),
-        'Integrated Gradients': ("IntegratedGradients", {}),
-        # 'Feature Permutation': ("FeaturePermutation", {})
+        # "Lime+Lasso": {
+        #     "method": "Lime",
+        #     "xai_lib": 'captum',
+        #     "device": TORCH_DEVICE,
+        # },
+        # "Lime+LR": {
+        #     "method": "Lime",
+        #     "xai_lib_kwargs": {
+        #         "interpretable_model": SkLearnLinearRegression()
+        #     },
+        #     "xai_lib": 'captum',
+        #     "device": TORCH_DEVICE,
+        # },
+        'IntegratedGradients': {
+            "method": "IntegratedGradients",
+            "xai_lib": 'captum',
+            "device": TORCH_DEVICE,
+        },
+        # 'FeaturePermutation': ("FeaturePermutation", {})
     })
 
     xai_metrics: dict = field(default_factory=lambda: {
         # Faithfulness
-        "Faithfulness Estimate": FaithfulnessEstimate(),  # works
-        "Monotonicity": Monotonicity(),  # works
-        "Selectivity": Selectivity(),  # works
-        "SensitivityN": CustomSensitivityN(),  # works
-        "Sufficiency": Sufficiency(),  # works
+        "Faithfulness Estimate": FaithfulnessEstimate(
+            perturb_baseline='mean',
+        ),
+        "Monotonicity": Monotonicity(
+            perturb_baseline='mean',
+        ),
+        "SensitivityN": CustomSensitivityN(
+            perturb_baseline='mean',
+        ),
+        "Sufficiency": Sufficiency(),
 
         # Robustness (Takes a long time)
-        "Avg Sensitivity": AvgSensitivity(),  # works
-        "Local Lipschitz Estimate": LocalLipschitzEstimate(),  # works
-        "Max Sensitivity": MaxSensitivity(),  # works
+        "Avg Sensitivity": AvgSensitivity(
+            nr_samples=100,
+            display_progressbar=True,
+        ),
+        "Local Lipschitz Estimate": LocalLipschitzEstimate(
+            nr_samples=100,
+            display_progressbar=True,
+        ),
+        "Max Sensitivity": MaxSensitivity(
+            nr_samples=100,
+            display_progressbar=True,
+        ),
 
         # Complexity
-        "Complexity": Complexity(),  # works
-        "Effective Complexity": EffectiveComplexity(),  # TODO: works, but check outputs
-        "Sparseness": Sparseness(),  # works
+        "Complexity": Complexity(),
+        "Effective Complexity": EffectiveComplexity(),
+        "Sparseness": Sparseness(),
 
         # Sensitivity (Randomness)
-        "Efficient MPRT": EfficientMPRT(),  # works
-        "MPRT": MPRT(),  # TODO: works, but check outputs
-        "Smooth MPRT": SmoothMPRT(),  # TODO: works, but check outputs
+        "Efficient MPRT": EfficientMPRT(),
+        "MPRT": MPRT(
+            return_aggregate=True,
+            return_average_correlation=True,
+        ),
+        "Smooth MPRT": SmoothMPRT(
+            nr_samples=100,
+            return_aggregate=True,
+            return_average_correlation=True,
+        ),
 
         # Axiomatic
-        "Completeness": Completeness(),  # TODO: works, check outputs
-        "Non-Sensitivity": NonSensitivity(),  # TODO: works, check outputs
+        "Completeness": Completeness(
+            perturb_baseline='mean',
+        ),
+        # "Non-Sensitivity": NonSensitivity(
+        #     perturb_baseline='mean',
+        # ),
     })
--- experiments/xai/XAIManager.py
+++ experiments/xai/XAIManager.py
@@ -1,24 +1,28 @@
-import json
+import marshal
 import os
-import time
-import warnings
+import pickle
+import types
+from functools import partial
+from multiprocessing import Pool
 
 import numpy as np
+import pandas as pd
 import quantus
 import torch
-from torch.utils.data import DataLoader, Dataset
+import tqdm
+from torch.utils.data import DataLoader
 
 from .XAIConfig import XAIConfig
 from ..model.LRClassifier import LRClassifier
 from ..model.NNClassifier import NNClassifier
-from ..utils import RESULTS_PATH, PRINT_WIDTH
+from ..utils import RESULTS_PATH
 
 if torch.cuda.is_available():
-    TORCH_DEVICE = torch.device("cuda:0")
+    TORCH_DEVICE = "cuda"
 elif torch.backends.mps.is_available():
-    TORCH_DEVICE = torch.device("mps")
+    TORCH_DEVICE = "mps"
 else:
-    TORCH_DEVICE = torch.device("cpu")
+    TORCH_DEVICE = "cpu"
 
 torch.manual_seed(42)
 
@@ -34,83 +38,101 @@ def remove_model_prefix(state_dict, prefix: str):
     return new_state_dict
 
 
+def evaluate_model_explanations(data: np.array, model: torch.nn.Module, xai_metrics: dict, xai_methods: dict,
+                                number_of_classes: int) -> pd.DataFrame:
+    target_index_to_explain = np.repeat(np.arange(number_of_classes), len(data), axis=0)
+
+    eval_scores = quantus.evaluate(
+        metrics=xai_metrics,
+        xai_methods=xai_methods,
+        model=model,
+        x_batch=data,
+        y_batch=target_index_to_explain,
+        agg_func=np.mean,
+        verbose=True,
+        return_as_df=True,
+        explain_func_kwargs={},
+        call_kwargs={'0': {'device': TORCH_DEVICE}},
+    )
+
+    return eval_scores
+
+
+def xai_task(data: np.array, model_file: os.PathLike,
+             number_of_features: int, number_of_classes: int):
+    if not os.path.exists(model_file):
+        raise ValueError(f"Model path {model_file} does not exist!")
+
+    if 'lr_model' in str(model_file):
+        model = LRClassifier(number_of_features, number_of_classes)
+    elif 'nn_model' in str(model_file):
+        model = NNClassifier(number_of_features, number_of_classes)
+    else:
+        raise ValueError(f"Model path {model_file} is not a valid model path!")
+    model.load_state_dict(remove_model_prefix(torch.load(model_file), '_module.'))
+
+    print(f"Evaluating model {model_file}...")
+    config = XAIConfig()
+    eval_scores = evaluate_model_explanations(data, model, config.xai_metrics, config.methods, number_of_classes)
+
+    model_xai_file = os.path.basename(model_file)[:-3] + 'csv'
+    eval_scores.to_csv(RESULTS_PATH / 'xai' / model_xai_file)
+
+
+def run_task(*args, **kwargs):
+    marshaled = kwargs.pop('marshaled_func')
+    func = marshal.loads(marshaled)
+    pickled_closure = kwargs.pop('pickled_closure')
+    pickled_closure = pickle.loads(pickled_closure)
+
+    restored_function = types.FunctionType(func, globals(), closure=create_closure(func, pickled_closure))
+    return restored_function(*args, **kwargs)
+
+
+def create_closure(func, original_closure):
+    indent = " " * 4
+    closure_vars_def = f"\n{indent}".join(f"{name}=None" for name in func.co_freevars)
+    closure_vars_ref = ",".join(func.co_freevars)
+    dynamic_closure = "create_dynamic_closure"
+    s = (f"""
+def {dynamic_closure}():
+    {closure_vars_def}
+    def internal():
+        {closure_vars_ref}
+    return internal.__closure__
+    """)
+    exec(s)
+    created_closure = locals()[dynamic_closure]()
+    for closure_var, value in zip(created_closure, original_closure):
+        closure_var.cell_contents = value
+    return created_closure
+
+
+def wrap_task(pool: Pool, task, generator):
+    closure = task.__closure__
+    pickled_closure = pickle.dumps(tuple(x.cell_contents for x in closure))
+    marshaled_func = marshal.dumps(task.__code__)
+    with tqdm.tqdm(total=len(generator)) as pbar:
+        for _ in pool.imap_unordered(partial(run_task, marshaled_func=marshaled_func, pickled_closure=pickled_closure),
+                                     generator):
+            pbar.update()
+
+
 class XAIManager:
     def __init__(self, config: XAIConfig = XAIConfig()):
         self.config = config
 
-    def _evaluate_model_explanations(self, data: Dataset, model: torch.nn.Module,
-                                     number_of_classes: int) -> dict[str, dict[str, float]]:
-        eval_scores = {}
-        target_index_to_explain = np.repeat(np.arange(number_of_classes), len(data['classes']), axis=0)
-
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore", category=UserWarning)
-
-            for xai_method_readable_name, (xai_method_name, xai_lib_kwargs) in self.config.methods.items():
-                print(f"Evaluating {xai_method_readable_name}...")
-                explain_function_kwargs = {
-                    "method": xai_method_name,
-                    "xai_lib_kwargs": xai_lib_kwargs,
-                    "device": TORCH_DEVICE,
-                }
-
-                attributions = quantus.explain(model, data['features'], target_index_to_explain,
-                                               **explain_function_kwargs)
-
-                xai_method_scores = {}
-                for xai_metric_name, xai_metric in self.config.xai_metrics.items():
-                    print(f"Starting {xai_metric_name} evaluation...", end='')
-                    pre_evaluation_time = time.time()
-                    xai_metric_results = xai_metric(
-                        model, data['features'], target_index_to_explain, attributions,
-                        explain_func=quantus.explain, explain_func_kwargs=explain_function_kwargs,
-                        device=TORCH_DEVICE,
-                    )
-                    print(f" {time.time() - pre_evaluation_time:.2f}s")
-
-                    if xai_metric_name in ["MPRT", "Smooth MPRT"]:
-                        xai_metric_results = xai_metric_results['output_layer']
-                    elif isinstance(xai_metric_results[0], np.ndarray):
-                        xai_metric_results = [np.average(x) for x in xai_metric_results]
-                    xai_method_scores[xai_metric_name] = np.average(np.squeeze(xai_metric_results))
-                eval_scores[xai_method_readable_name] = xai_method_scores
-
-        return eval_scores
-
     def evaluate_explanations(self, data: DataLoader, number_of_features: int, number_of_classes: int,
                               model_paths: list[os.PathLike]) -> None:
         data = next(iter(data))
+        xai_test_samples = data['features']
+        if self.config.number_of_xai_test_samples > 0:
+            xai_test_samples = xai_test_samples[np.random.choice(len(data['features']),
+                                                                 self.config.number_of_xai_test_samples, replace=False)]
 
-        done_models = [p for p in os.listdir(RESULTS_PATH / 'xai') if p.endswith('.json')]
+        def task_wrapper(model_path: os.PathLike) -> None:
+            xai_task(data=xai_test_samples, model_file=model_path,
+                     number_of_features=number_of_features, number_of_classes=number_of_classes)
 
-        for model_file in model_paths:
-            model_xai_file = os.path.basename(model_file)[:-3] + 'json'
-            if model_xai_file in done_models:
-                continue
-
-            if not os.path.exists(model_file):
-                raise ValueError(f"Model path {model_file} does not exist!")
-
-            if 'lr_model' in str(model_file):
-                model = LRClassifier(number_of_features, number_of_classes)
-                continue
-            elif 'nn_model' in str(model_file):
-                model = NNClassifier(number_of_features, number_of_classes)
-            else:
-                raise ValueError(f"Model path {model_file} is not a valid model path!")
-            model.load_state_dict(remove_model_prefix(torch.load(model_file), '_module.'))
-
-            print(f"Evaluating model {model_file}...")
-            eval_scores = self._evaluate_model_explanations(data, model, number_of_classes)
-
-            with open(RESULTS_PATH / 'xai' / model_xai_file, 'w') as f:
-                json.dump(eval_scores, f)
-
-            print(f" Model name: {str(model_file).split('/')[-1][:-3]} ".center(PRINT_WIDTH, '-'))
-            for metric_name, xai_metric_scores in eval_scores.items():
-                print(f"{metric_name}".center(PRINT_WIDTH, '_'))
-                for meta_metric_name, meta_metric_score in xai_metric_scores.items():
-                    print(f"{meta_metric_name}: {meta_metric_score}, ", end='')
-                print()
-            print()
-            print()
+        with Pool(self.config.processes) as p:
+            wrap_task(p, task_wrapper, model_paths)
